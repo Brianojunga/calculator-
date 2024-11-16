cac7d0b3cc7fc5e18137bcb807fb065ed8f596b7
Brianojunga <brianojunga3@gmail.com>
2024-11-16 22:22:09 +0300

    Add majority of functionality except the delete and percentage buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let firstNum = "";
 let operator = "";
 let secondNum = '';
-let isOperator = false;
+let isFirstNum = false;
+let isError = false;
 
 const dot = document.querySelector(".itemdot");
 const plus = document.querySelector(".itemadd");
@@ -12,55 +13,92 @@ const equals = document.querySelector(".itemRes");
 const del = document.querySelector(".itemdel");
 const reset = document.querySelector(".itemAc");
 const percentage = document.querySelector(".itemper");
-const showinput = document.querySelector(".userInput");
+const showInput = document.querySelector(".user-input");
+const showResult = document.querySelector(".show-result");
 
 
 
 function getNum(num){
+  if(isError) return
+
     if(operator === ""){ if(num==="." && firstNum.includes(".") )return;
+      if(isFirstNum == true){
+         firstNum = "";
+        showInput.textContent = num;
+        isFirstNum = false;
+       }else{
         firstNum += num;
+        showInput.textContent += num;
+      }
+        
     }else{
-        if(num==="." && firstNum.includes(".") )return;
+        if(num==="." && secondNum.includes(".") )return;
         secondNum += num
+        showInput.textContent += num;
     } 
 }
 
 
 function getOperator(op){
-    if(operator == ""){
+
+  if(isError)return
+
+    if(operator === ""){
         operator = op;
-       // isOperator = true;
-    }else{
-        const result = calculate();
-        firstNum = result;
+        showInput.textContent += op;}
+    else{
+      if (secondNum === "") {
         operator = op;
-        secondNum = "";
-    }
-}
+        showInput.textContent = showInput.textContent.slice(0, -1) + op;
+      }
+         else{
+          const result = calculate();
+          if (result === "Math Error") {
+            showResult.textContent = result;
+            firstNum = "";
+            operator = "";
+            secondNum = "";
+            showInput.textContent = "";
+            isError = true;
+            }
+         else{
+            showResult.textContent = result;
+            firstNum = parseFloat(result.toFixed(8));
+            showInput.textContent = firstNum + op;
+            operator = op;
+            showInput.textContent += op;
+            secondNum = "";
+         }
+         }
+         
+      }
+    }  
 
 function calculate(){
-    if (firstNum === ""){
-        firstNum = 0;
-    }
-    if(secondNum == ""){
-        secondNum = firstNum;
-    };
-    if(operator === "+"){
-        return parseFloat(firstNum) + parseFloat(secondNum)
-    }
-    else if(operator === "*"){
-        return parseFloat(firstNum) * parseFloat(secondNum)
-    }
-    else if(operator === "-"){
-        return parseFloat(firstNum) - parseFloat(secondNum)}
-    else if(operator === "/"){
-        if(secondNum !== "0"){
-           return parseFloat(firstNum)/parseFloat(secondNum) 
-        }else{
-          return  "Error"
-        } 
-    }
+  if(operator === "" && secondNum === "") return firstNum
+
+  if(operator === "+" || operator === "-"){
+    if(firstNum === "") {firstNum = "0"};
+    if (secondNum === ""){secondNum = "0"}
+
+    return operator === "+" 
+    ?parseFloat(firstNum) + parseFloat(secondNum)
+    :parseFloat(firstNum) - parseFloat(secondNum)
+}else{
+  if(firstNum === "" || secondNum === ""){
+    return "Math Error"
+  }
+
+  if(operator === "*"){
+    return parseFloat(firstNum) * parseFloat(secondNum)
+  }else{
+    return secondNum ==="0"
+    ? "Math Error"
+    : parseFloat(firstNum) / parseFloat(secondNum)
+  }
 }
+}
+
 
 
 
@@ -69,4 +107,44 @@ dot.addEventListener("click", () => {
 })
 plus.addEventListener("click", () => {
     getOperator("+");
+})
+
+subtraction.addEventListener("click", () => {
+    getOperator("-");
+})
+
+multiplication.addEventListener("click", () => {
+    getOperator("*");
+})
+division.addEventListener("click", () => {
+    getOperator("/");
+})
+equals.addEventListener("click", () => {
+    let result = calculate()
+    if (result === "Math Error") {
+      showResult.textContent = result;
+      firstNum = "";
+      operator = "";
+      secondNum = "";
+      showInput.textContent = "";
+      isError = true;
+      }
+     else{
+        showResult.textContent = result;
+        firstNum = parseFloat(result.toFixed(8));
+        showInput.textContent = firstNum;
+        isFirstNum = true;
+        secondNum = "";
+        operator = "";
+     };   
+})
+reset.addEventListener("click", () => {
+    firstNum = "";
+    operator = "";
+    secondNum = '';
+    isFirstNum = false;
+    showInput.textContent = "";
+    showResult.textContent = 0;
+    isError = false;
+
 })
